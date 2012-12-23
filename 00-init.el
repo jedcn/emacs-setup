c5d00e4f5f4b69a1e279e06a4bb378e4125857ad
Jed Northridge <northridge@gmail.com>
2012-12-23 14:41:49 -0500

    [packages] Add starter-kit-lisp
    * Missed paredit.

--- 00-init.el
+++ 00-init.el
@@ -5,7 +5,7 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit)
+(defvar my-packages '(starter-kit starter-kit-lisp)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
