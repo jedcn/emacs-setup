38fcf1b4da25beb97a1e0ffaf59b40e2874a7d52
Jed Northridge <northridge@gmail.com>
2012-12-23 14:20:31 -0500

    [key bindings] leftovers

---
+++ someday.el
@@ -0,0 +1,10 @@
+;; keybindings
+
+;; Should be able to eval-and-replace anywhere.
+;; (global-set-key (kbd "C-c e") 'esk-eval-and-replace)
+
+;; M-S-6 is awkward
+;; (global-set-key (kbd "C-c q") 'join-line)
+
+;;  (global-set-key (kbd "C-M-s") 'isearch-forward)
+;;  (global-set-key (kbd "C-M-r") 'isearch-backward)
