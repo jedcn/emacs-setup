f8d3d7186f8f8541940467c1eab868da101b26bd
Jed Northridge <northridge@gmail.com>
2012-12-28 21:49:35 -0500

    [appearance] It looks like Emacs Rocks now! Alright!

--- mac/setup-mac.el
+++ mac/setup-mac.el
@@ -2,8 +2,6 @@
 ;;
 ;; https://raw.github.com/magnars/.emacs.d/master/mac.el
 (progn
-  (set-face-attribute 'default nil :font "Monaco-16")
-
   (setq mac-option-modifier 'none)
   (setq mac-command-modifier 'meta)
   (setq ns-function-modifer 'hyper))
---
+++ misc/appearance.el
@@ -0,0 +1,25 @@
+(custom-set-faces
+ ;; TODO: Deconstruct..
+ '(highlight ((((class color) (min-colors 88) (background dark)) (:background "#111111")))))
+
+;; Prefer dark backgrounds with white text
+(set-face-background 'default "Black")
+(set-face-foreground 'default "White")
+
+;; TODO: Document. 'region described the background color that is
+;; shown when you move your cursor away from a region?
+(set-face-background 'region "#464740")
+(set-face-foreground 'font-lock-warning-face "#ff6666")
+
+;; Matching parents get blue-ish, mismatched go red
+(set-face-background 'show-paren-match "#333399")
+(set-face-background 'show-paren-mismatch "Red")
+
+(when window-system
+  ;; Make the title of emacs reflect the buffer being edited
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  ;; Make the cursor blink when inactive
+  (blink-cursor-mode 1))
+
+;; Mac friendly font
+(set-face-attribute 'default nil :font "Monaco-16")
