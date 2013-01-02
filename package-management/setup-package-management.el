9b02087b20cd087dac0a43574da7d1a75226d3b1
Jed Northridge <northridge@gmail.com>
2013-01-01 20:51:04 -0500

    [packages] No more solarized-theme
    * Just didn't use..

--- package-management/setup-package-management.el
+++ package-management/setup-package-management.el
@@ -9,7 +9,7 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit starter-kit-lisp yasnippet rvm rspec-mode solarized-theme dired-details)
+(defvar my-packages '(starter-kit starter-kit-lisp yasnippet rvm rspec-mode dired-details)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
