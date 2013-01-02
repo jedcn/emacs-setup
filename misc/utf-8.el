01bbac7623fae2419e1c2de3b81b5e997a8bae57
Jed Northridge <northridge@gmail.com>
2013-01-01 20:44:45 -0500

    [utf8] Always prefer UTF8

---
+++ misc/utf-8.el
@@ -0,0 +1,9 @@
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; See:
+;;
+;; https://github.com/magnars/.emacs.d/blob/master/sane-defaults
