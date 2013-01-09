aabdfb14232f4bbc96996d4b9fca5d977ef4178b
Jed Northridge <northridge@gmail.com>
2013-01-08 20:01:31 -0500

    [key bindings] Setup rspec bindings
    Can run..
    
    * all rspecs in project,
    * all examples in file,
    * the example under the cursor

--- rspec-mode/setup-rspec-mode.el
+++ rspec-mode/setup-rspec-mode.el
@@ -5,3 +5,7 @@
   (let ((shell-file-name "/bin/bash"))
     ad-do-it))
 (ad-activate 'rspec-compile)
+
+(global-set-key (kbd "C-, a") 'rspec-verify-all)
+(global-set-key (kbd "C-, f") 'rspec-verify)
+(global-set-key (kbd "C-, l") 'rspec-verify-single) ;; mnemonic: l = line
