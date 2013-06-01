ed315ae2650a4378edf388122483f6792034cca3
Jed Northridge <northridge@gmail.com>
2013-06-01 19:36:30 -0400

    [literate, goto-line] Converted goto-line functionality

--- key-bindings.el
+++ key-bindings.el
@@ -4,6 +4,19 @@
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
+(global-set-key (kbd "C-x g") 'goto-line)
+
+(global-set-key [remap goto-line] 'goto-line-with-feedback)
+
+(defun goto-line-with-feedback ()
+  "Show line numbers temporarily, while prompting for the line number input"
+  (interactive)
+  (unwind-protect
+      (progn
+        (linum-mode 1)
+        (goto-line (read-number "Goto line: ")))
+    (linum-mode -1)))
+
 (global-set-key (kbd "C-x m") 'magit-status)
 
 (setq mac-command-modifier 'meta)
