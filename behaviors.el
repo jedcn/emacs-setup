0912c89b5885af9f33d6fcd74d96649e8e4ebb66
Jed Northridge <northridge@gmail.com>
2013-06-01 16:48:47 -0400

    [literate] Added 'behaviors.org,' a file about hooks

--- behaviors.el
+++ behaviors.el
@@ -1,7 +1,4 @@
-;; Make it so that trailing whitespace is removed and tabs are
-;; converted to spaces every time you save.
-;;
-;; See: http://whattheemacsd.com/buffer-defuns.el-01.html
+
 (defun cleanup-buffer-safe ()
   "Perform a bunch of safe operations on the whitespace content of a buffer."
   (interactive)
