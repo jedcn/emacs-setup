c68755c5a9aebe31f4f3d4e778c7b685b6daddde
Jed Northridge <northridge@gmail.com>
2013-06-04 13:49:45 -0400

    [literate] Added package configuration

--- general-setup.el
+++ general-setup.el
@@ -1,4 +1,9 @@
 
+(package-initialize)
+
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (set-face-attribute 'default nil :font "Menlo-18")
 
 (setq locale-coding-system 'utf-8)
