13e6f490939a75ec9c37b48015b57f81542bf31e
Jed Northridge <northridge@gmail.com>
2013-06-09 15:56:44 -0400

    [ruby-mode] Enhanced list of file types to be edited with ruby-mode

--- modes.el
+++ modes.el
@@ -38,9 +38,15 @@ If REPOSITORY is specified, use that."
     ad-do-it))
 (ad-activate 'rspec-compile)
 
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile$" . ruby-mode))
 
 (require 'magit)
 
