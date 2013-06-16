c317b6056002d252de09b6525ddd5a3033ee8863
Jed Northridge <northridge@gmail.com>
2013-06-15 23:41:47 -0400

    [key-bindings] Bring back C-o shows hits while searching

--- general-setup.el
+++ general-setup.el
@@ -19,3 +19,8 @@
 (set-face-attribute 'default nil :font "Menlo-18")
 
 (set-face-attribute 'font-lock-comment-face nil :foreground "#3f7f5f")
+
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
