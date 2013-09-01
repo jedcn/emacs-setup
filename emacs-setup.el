10598394e39cb06fef337047ad303f40014104c4
Jed Northridge <northridge@gmail.com>
2013-09-01 10:58:45 -0400

    [magit] Use green/red within magit for add/removal

--- emacs-setup.el
+++ emacs-setup.el
@@ -177,6 +177,11 @@ If REPOSITORY is specified, use that."
 
 (sacha/package-install 'magit)
 
+(eval-after-load 'magit
+  '(progn
+     (set-face-foreground 'magit-diff-add "green4")
+     (set-face-foreground 'magit-diff-del "red3")))
+
 (require 'magit)
 
 (defadvice magit-status (around magit-fullscreen activate)
