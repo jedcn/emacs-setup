a5e13b055f5da125fc1697ad847ea5d14ae79882
Jed Northridge <northridge@gmail.com>
2013-11-21 06:23:03 -0500

    [magit] Hunk diffs are now easier to visually parse

--- emacs-setup.el
+++ emacs-setup.el
@@ -247,6 +247,8 @@ If REPOSITORY is specified, use that."
 
 (setq magit-emacsclient-executable "/usr/local/bin/emacsclient")
 
+(setq magit-diff-refine-hunk 'all)
+
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
