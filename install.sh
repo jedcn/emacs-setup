848e7117d3bcd9ad3930a467dcc25e9d47a426d0
Jed Northridge <northridge@gmail.com>
2014-05-17 21:52:28 -0400

    [cask] Added first round of Cask files
    I'm using pallet, described here: https://github.com/rdallasgray/pallet

--- install.sh
+++ install.sh
@@ -8,4 +8,12 @@ echo "Creating $HOME/.emacs.d/emacs-setup as link to $emacs_setup_dir"
 ln -s $emacs_setup_dir $HOME/.emacs.d/emacs-setup
 
 echo "Creating $HOME/.emacs.d/init.el"
-echo '(load (concat user-emacs-directory "emacs-setup/emacs-setup.el"))' >> $HOME/.emacs.d/init.el
+cp init.el $HOME/.emacs.d
+
+echo "Creating $HOME/.emacs.d/Cask"
+cp Cask $HOME/.emacs.d
+
+pushd $HOME/.emacs.d
+echo "Installing Packages with Cask"
+cask install
+popd
